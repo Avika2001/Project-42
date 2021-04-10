ef275c40c15a41018b62c86d9c775756675b1065
Avika2001 <75854853+Avika2001@users.noreply.github.com>
2021-04-10 20:55:15 +0100

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -3,11 +3,11 @@ class Umbrella {
         var options = {
             isStatic: true,
         }
-        this.image = loadImage("Walking Frame/walking_1.png");
+        this.image = loadImage("walking_1.png");
         this.umbrella = Bodies.circle(x,y,50,options);
         this.radius = 50;
         World.add(world, this.umbrella)
-        this.batmanImg = loadImage("Walking Frame/batman.png");
+        this.batmanImg = loadImage("batman.png");
         
     }
 
